e52cebc0663a9568ec47845d1962ef3dfbd9b72a
Roberto Sánchez <rsa568422@gmail.com>
2025-08-19 22:20:17 +0200

    Cambios menores

--- infrastructure-spring/src/main/java/com/example/microservice/spring/configuration/ServiceConfiguration.java
+++ infrastructure-spring/src/main/java/com/example/microservice/spring/configuration/ServiceConfiguration.java
@@ -1,24 +1,39 @@
 package com.example.microservice.spring.configuration;
 
+import com.example.microservice.application.mapper.SchedulerMapper;
 import com.example.microservice.application.mapper.TaskMapper;
 import com.example.microservice.application.mapper.WorkerMapper;
+import com.example.microservice.application.port.out.SchedulerRepository;
 import com.example.microservice.application.port.out.TaskRepository;
 import com.example.microservice.application.port.out.WorkerRepository;
+import com.example.microservice.application.service.SchedulerService;
 import com.example.microservice.application.service.TaskService;
 import com.example.microservice.application.service.WorkerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ServiceConfiguration {
 
+    private final TaskRepository taskRepository;
+
+    private final WorkerRepository workerRepository;
+
     @Bean
-    public TaskService taskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+    public SchedulerService schedulerService(SchedulerRepository schedulerRepository,
+                                             SchedulerMapper schedulerMapper) {
+        return new SchedulerService(schedulerRepository, taskRepository, workerRepository, schedulerMapper);
+    }
+
+    @Bean
+    public TaskService taskService(TaskMapper taskMapper) {
         return new TaskService(taskRepository, taskMapper);
     }
 
     @Bean
-    public WorkerService workerService(WorkerRepository workerRepository, WorkerMapper workerMapper) {
+    public WorkerService workerService(WorkerMapper workerMapper) {
         return new WorkerService(workerRepository, workerMapper);
     }
 }
